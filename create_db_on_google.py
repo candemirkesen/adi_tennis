2d686c3ddb87d79f881bfed64e48ff85675a99db
Demirkesen <Can.Demirkesen@analog.com>
2022-11-01 17:50:24 +0300

    cloud settings completed

--- create_db_on_google.py
+++ create_db_on_google.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 
 mydb = mysql.connector.connect(
-    host="34.159.140.186", # 34.159.140.186 34.118.20.170
+    host="34.118.109.6",
     user="root",
     password="123456",
     database="score_table"
@@ -17,13 +17,13 @@ my_cursor = mydb.cursor()
 
 # my_cursor.execute("SHOW DATABASES")
 
-my_cursor.execute("CREATE TABLE scores (id INT AUTO_INCREMENT PRIMARY KEY, player VARCHAR(255), score INT)")
+# my_cursor.execute("CREATE TABLE scores (id INT AUTO_INCREMENT PRIMARY KEY, player VARCHAR(255), score INT)")
 
 # my_cursor.execute("SELECT * FROM scores")
 
-# my_cursor.execute("DROP TABLE scores")
+my_cursor.execute("DROP TABLE scores")
 
-# my_cursor.execute("CREATE TABLE scores (id INT AUTO_INCREMENT PRIMARY KEY, player VARCHAR(255), score INT)")
+my_cursor.execute("CREATE TABLE scores (id INT AUTO_INCREMENT PRIMARY KEY, player VARCHAR(255), score INT)")
 
 
 # my_cursor.execute("SHOW TABLES")
@@ -57,3 +57,135 @@ for x in my_cursor:
     print(x)
 
 
+# export GOOGLE_CLOUD_PROJECT=scores-364816
+
+# mkdir some_dir && cd some_dir && nano main.tf
+
+import os
+
+import sqlalchemy
+
+
+# connect_unix_socket initializes a Unix socket connection pool for
+# a Cloud SQL instance of MySQL.
+def connect_unix_socket() -> sqlalchemy.engine.base.Engine:
+    # Note: Saving credentials in environment variables is convenient, but not
+    # secure - consider a more secure solution such as
+    # Cloud Secret Manager (https://cloud.google.com/secret-manager) to help
+    # keep secrets safe.
+    db_user = os.environ["root"]  # e.g. 'my-database-user'
+    db_pass = os.environ["123456"]  # e.g. 'my-database-password'
+    db_name = os.environ["score_table"]  # e.g. 'my-database'
+    unix_socket_path = os.environ["/cloudsql/scores-364816:europe-central2:adi-tennis-db"]  # e.g. '/cloudsql/project:region:instance'
+
+    pool = sqlalchemy.create_engine(
+        # Equivalent URL:
+        # mysql+pymysql://<db_user>:<db_pass>@/<db_name>?unix_socket=<socket_path>/<cloud_sql_instance_name>
+        sqlalchemy.engine.url.URL.create(
+            drivername="mysql+pymysql",
+            username=db_user,
+            password=db_pass,
+            database=db_name,
+            query={"unix_socket": unix_socket_path},
+        ),
+        # ...
+    )
+    return pool
+
+
+
+#     gcloud sql instances describe adi-tennis-db
+
+#     backendType: SECOND_GEN
+
+
+# backendType: SECOND_GEN
+# connectionName: scores-364816:europe-central2:adi-tennis-db
+# createTime: '2022-10-21T21:35:56.249Z'
+# databaseInstalledVersion: MYSQL_8_0_26
+# databaseVersion: MYSQL_8_0
+# etag: b7ca5b0a448f1fe97bf616ef5091f97ebd3398d08345a98b7812b6acd747f617
+# failoverReplica:
+#   available: true
+# gceZone: europe-central2-a
+# instanceType: CLOUD_SQL_INSTANCE
+# ipAddresses:
+# - ipAddress: 34.118.20.170
+#   type: PRIMARY
+# kind: sql#instance
+# maintenanceVersion: MYSQL_8_0_26.R20220809.02_02
+# name: adi-tennis-db
+# project: scores-364816
+# region: europe-central2
+# secondaryGceZone: europe-central2-b
+# selfLink: https://sqladmin.googleapis.com/sql/v1beta4/projects/scores-364816/instances/adi-tennis-db
+# serverCaCert:
+#   cert: |-
+#     -----BEGIN CERTIFICATE-----
+#     MIIDfzCCAmegAwIBAgIBADANBgkqhkiG9w0BAQsFADB3MS0wKwYDVQQuEyRjYmVh
+#     OWJiZi1iZDIwLTQwOWYtYjRmZS01NzA4NTUyOTM0NGMxIzAhBgNVBAMTGkdvb2ds
+#     ZSBDbG91ZCBTUUwgU2VydmVyIENBMRQwEgYDVQQKEwtHb29nbGUsIEluYzELMAkG
+#     A1UEBhMCVVMwHhcNMjIxMDIxMjEzOTM2WhcNMzIxMDE4MjE0MDM2WjB3MS0wKwYD
+#     VQQuEyRjYmVhOWJiZi1iZDIwLTQwOWYtYjRmZS01NzA4NTUyOTM0NGMxIzAhBgNV
+#     BAMTGkdvb2dsZSBDbG91ZCBTUUwgU2VydmVyIENBMRQwEgYDVQQKEwtHb29nbGUs
+#     IEluYzELMAkGA1UEBhMCVVMwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIB
+#     AQCjUvowrQQv4U/NbeXNbfVYpPbE7Pe+FyZaZQ8kVVZbS7WrIL6s+OyNVydUOuII
+#     37PfdH5y3MV2GrfH2O1yUhoKakUlXMY/xGh+1ePvQd94aWKMM/dx/BCylT+6kH2D
+#     k2Nbo2hrTtxWw9tOVthrprTTptlDe3FS744QdPGwubiF7PbFsS4YpRY2si7YCTgN
+#     HTrv5vLQEopbOHlDgqZRthpJfoZip/jfm1zyzNI2NrmonGyD0OtA7J9pXP6d0AZz
+#     XEzejvy+cBZ3jb2EKoqiUzq2IxnaYMX65eSHJwYNs8hTO3AlXuy85xkD2gvibt5E
+#     s3adj4XQ+l5ONF+wu6YrKCDRAgMBAAGjFjAUMBIGA1UdEwEB/wQIMAYBAf8CAQAw
+#     DQYJKoZIhvcNAQELBQADggEBADd7E0f67cLrX5RW1o15VWIl5OT3yWxPXK8m1XHf
+#     i7LWuO2HR9q2eXQfgqDYcbiIS4MtrrQaWWKK1rv6noEOMhftLHGFmSKp/76dy7op
+#     92yVmvnK1BhIa9vGSGNp3dAfi/kLjDUCohWh3TYQB00gqzgWtBap/MdRdLE0SWxN
+#     /8kDmZ0X4YUiGlKQS+hASrAlxyGKHdGJVfWloe93S4H9A/lZGikxn4wK0utsMbpV
+#     VIlMu8vmLlUL43w5fqoT4H6ZExLCwmZqz0eIwNdx9fNcPla3aEp/x46lJa0ZUO0O
+#     FUQ/hU/zKOX/huCb9qI4KTrwQYNFl85GLp+s4gnDvBgssro=
+#     -----END CERTIFICATE-----
+#   certSerialNumber: '0'
+#   commonName: C=US,O=Google\, Inc,CN=Google Cloud SQL Server CA,dnQualifier=cbea9bbf-bd20-409f-b4fe-57085529344c
+#   createTime: '2022-10-21T21:39:36.657Z'
+#   expirationTime: '2032-10-18T21:40:36.657Z'
+#   instance: adi-tennis-db
+#   kind: sql#sslCert
+#   sha1Fingerprint: 2bd6acccaf67bfd800e55e4519fd77905add4387
+# serviceAccountEmailAddress: p549690191909-nvs05x@gcp-sa-cloud-sql.iam.gserviceaccount.com
+# settings:
+#   activationPolicy: ALWAYS
+#   availabilityType: REGIONAL
+#   backupConfiguration:
+#     backupRetentionSettings:
+#       retainedBackups: 7
+#       retentionUnit: COUNT
+#     binaryLogEnabled: true
+#     enabled: true
+#     kind: sql#backupConfiguration
+#     location: eu
+#     startTime: 14:00
+#     transactionLogRetentionDays: 7
+#   connectorEnforcement: NOT_REQUIRED
+#   dataDiskSizeGb: '100'
+#   dataDiskType: PD_SSD
+#   deletionProtectionEnabled: true
+#   ipConfiguration:
+#     authorizedNetworks:
+#     - kind: sql#aclEntry
+#       name: ''
+#       value: 24.133.128.152
+#     ipv4Enabled: true
+#   kind: sql#settings
+#   locationPreference:
+#     kind: sql#locationPreference
+#     zone: europe-central2-a
+#   maintenanceWindow:
+#     day: 0
+#     hour: 0
+#     kind: sql#maintenanceWindow
+#     updateTrack: stable
+#   pricingPlan: PER_USE
+#   replicationType: SYNCHRONOUS
+#   settingsVersion: '21'
+#   storageAutoResize: true
+#   storageAutoResizeLimit: '0'
+#   tier: db-custom-4-26624
+# state: RUNNABLE
--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for
-# from flask_wtf import FlaskForm
+from flask_wtf import FlaskForm
 from flask_sqlalchemy import SQLAlchemy
-# from flask import session
-# from sqlalchemy.sql import text
+from flask import session
+from sqlalchemy.sql import text
 import sqlalchemy
-import os
-# from sqlalchemy import create_engine
-# from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
-# from sqlalchemy import inspect
+from sqlalchemy import create_engine
+from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
+from sqlalchemy import inspect
 import pymysql
 
 # import mysql.connector
@@ -41,12 +40,11 @@ import pymysql
 #         return conn
 
 # Google Cloud SQL (change this accordingly)
-PASSWORD ='123456'
-PUBLIC_IP_ADDRESS ='34.118.20.170' # 34.118.20.170  34.159.140.186
+PASSWORD =''
+PUBLIC_IP_ADDRESS ='34.118.109.6'
 DBNAME ='score_table'
 PROJECT_ID ='scores-364816'
-INSTANCE_NAME ='scores-364816:europe-west3:mycloudsql'
-
+INSTANCE_NAME ='tennis-367213:europe-central2:tennisdb'
 
 # mydb = mysql.connector.connect(
 #     host="34.118.20.170",
@@ -56,46 +54,20 @@ INSTANCE_NAME ='scores-364816:europe-west3:mycloudsql'
 # )
 
 
-
-
-
 app = Flask(__name__)
 app.secret_key = "any-string-you-want-just-keep-it-secret"
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///adi_tennis.db'
 # app.config["SQLALCHEMY_DATABASE_URI"]= f"mysql+pymysql://root:123456@{PUBLIC_IP_ADDRESS}/{DBNAME}"
-# app.config["SQLALCHEMY_DATABASE_URI"]= f"mysql+pymysql://root:123456@{PUBLIC_IP_ADDRESS}/{DBNAME}"
-# app.config["SQLALCHEMY_DATABASE_URI"]= f"mysql+pymysql://root:123456@{INSTANCE_NAME}"
+# app.config["SQLALCHEMY_DATABASE_URI"]= f"mysql+pymysql://root:123456@/cloudsql/{INSTANCE_NAME}"
+app.config["SQLALCHEMY_DATABASE_URI"]= 'mysql+pymysql://root:123456@/score_table?unix_socket=/cloudsql/tennis-367213:europe-central2:tennisdb'
+# app.config["SQLALCHEMY_DATABASE_URI"]= f"mysql+pymysql://root:{INSTANCE_NAME}"
 # app.config["SQLALCHEMY_DATABASE_URI"]= f"mysql+pymysql://root:123456@localhost/tennis_scores"
 # app.config["SQLALCHEMY_DATABASE_URI"]= f"mysql://root:123456@localhost/tennis_scores"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:123456@score_table?unix_socket=/cloudsql/scores-364816:europe-central2:adi-tennis-db'
-
-engine = sqlalchemy.create_engine("mysql+pymysql://root:123456@34.159.140.186/score_table")
-# engine = sqlalchemy.create_engine("mysql+pymysql://root:123456@34.118.20.170/cloudsql/scores-364816:europe-central2:adi-tennis-db")
-
-app.config['SQLALCHEMY_DATABASE_URI'] = engine.url.render_as_string(hide_password=False)
-
+# /cloudsql/{your-cloudsql-connection-string}
 
 # engine = create_engine('mysql://root:123456@localhost/tennis_scores')
-
-# with engine.connect() as con:
-#     rs = con.execute('SELECT * FROM scores')
-#     for row in rs:
-#         print('ananananan')
-#         print(row)
-
-# print(rs)    
-# for row in rs:
-#     print('ananananan')
-#     print(row)
-
-# configure Flask-SQLAlchemy to use Python Connector
-# app.config['SQLALCHEMY_DATABASE_URI'] = "mysql + mysqldb://"
-# app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
-#     "creator": getconn
-# }
-
 
 db = SQLAlchemy(app)
 db.app = app
@@ -123,57 +95,16 @@ def updateScores_db(r_a, r_b): #r_a and r_b previous scores a and b. a is winner
   e_b = 1 / (1 + pow(10, -temp_var))
   return round(r_a + 20*(s_a-e_a)), round(r_b + 20*(s_b-e_b))
 
-def connect_unix_socket() -> sqlalchemy.engine.base.Engine:
-    # Note: Saving credentials in environment variables is convenient, but not
-    # secure - consider a more secure solution such as
-    # Cloud Secret Manager (https://cloud.google.com/secret-manager) to help
-    # keep secrets safe.
-    db_user = 'root' #os.environ["root"]  # e.g. 'my-database-user'
-    db_pass = '123456' # os.environ["123456"]  # e.g. 'my-database-password'
-    db_name = 'score_table' #os.environ["score_table"]  # e.g. 'my-database'
-    unix_socket_path = '/cloudsql/scores-364816:europe-central2:adi-tennis-db' # os.environ["/cloudsql/scores-364816:europe-central2:adi-tennis-db"]  # e.g. '/cloudsql/project:region:instance'
-
-    pool = sqlalchemy.create_engine(
-        # Equivalent URL:
-        # mysql+pymysql://<db_user>:<db_pass>@/<db_name>?unix_socket=<socket_path>/<cloud_sql_instance_name>
-        sqlalchemy.engine.url.URL.create(
-            drivername="mysql+pymysql",
-            username=db_user,
-            password=db_pass,
-            database=db_name,
-            query={"unix_socket": unix_socket_path},
-        ),
-        # ...
-    )
-    return pool
-
-# pool = connect_unix_socket()
-# print(f"pool is : {pool}")
-# pool.connect()
-
 
 @app.route("/", methods=["GET", "POST"])
 def home():
-
-    # connect_unix_socket()
 
     # if selection is None:
     winner = 'anan'
     loser = 'anan'
     players = scores.query.order_by(scores.score.desc())
-    # print(players)
-    # champion = scores.query.filter_by(player=loser).first()
-    champion = 'anan'
+    champion = scores.query.order_by(scores.score.desc()).first()
     # my_cursor.execute("SELECT * FROM scores")
-
-
-    # for i in range(len(df)):
-    #     player_name = df.Player[i]
-    #     player_score = df.Score[i]
-    #     new_game = TennisScores(player=player_name, score=int(player_score))
-    #     db.session.add(new_game)
-    #     db.session.commit()
-    # db.session.commit()
 
     if request.method == 'POST':
         winner = request.form['winner']
@@ -187,26 +118,7 @@ def home():
             winner_player.score, loser_player.score = updateScores_db(winner_player.score, loser_player.score)
             db.session.commit()
 
-
-        # with engine.connect() as con:
-        #     rs = con.execute('SELECT * FROM scores')
-
-        # print(rs)    
-        # for row in rs:
-        #     print('ananananan')
-        #     print(row)
-
-        # players = scores.query.order_by(scores.score)
-        # print(f'players: {players}')
-
-        # new_game = TennisScores(player=winner, score=1111)
-        # db.session.add(new_game)
-        # db.session.commit()
-
-
-    # idx = df['Score'].idxmax()
-    # champion = df.Player[idx]
-    # return f'Result: {players}'
+    champion = scores.query.order_by(scores.score.desc()).first()
     return render_template("homepage.html", winner=winner, loser=loser, champion=champion, players=players)
 
 
